70b552a071c54222f92914efa6cc53ccef0c29ab
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-12-13 20:04:13 +0300

    FinalFinal

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int addedBalance = 2000;
+        int addedBalance = 1100;
         int bonus = addedBalance / 100;
         int finalBalance = balance + addedBalance + bonus;
         if (addedBalance > 1000) {
             System.out.println( "бонус " + bonus + ", итоговый счет: " + finalBalance);
         } else {
-            System.out.println("бонусов нет, итоговая сумма " + finalBalance);
+            System.out.println("бонусов нет, итоговая сумма " + (balance + addedBalance));
         }
     }
 }
